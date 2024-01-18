7991626d507287d83a189a38cfc948f072f9f222
Jeshua Cienfuegos Estrada <Deku050204@gmail.com>
2024-01-18 12:38:00 -0600

    Actualización del Script "Player" con comentarios.
    Se agregaron comentarios al Script Player para ayudar a comprender mejor el código.

--- TEMA1-CEJ-PROGRAMACION2/Assets/16-01-2024/Scripts/Player.cs
+++ TEMA1-CEJ-PROGRAMACION2/Assets/16-01-2024/Scripts/Player.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    //Clase jugador base donde se crean variables publicas y protegidas.
+.
     public bool isDead = true;
     protected float health;
 
+    // Se crea una función que obtiene la variable Deeada para devolverla con otro nombre.
     public bool getIsdead()
     {
         return isDead;
@@ -20,15 +23,18 @@ public class Player : MonoBehaviour
 
 public class Heroe : Player
 {
-
+    // En está clase se crean variables privadas.
     private int HeroeAgil;
     //private float HeroeFuerte;
+
+    // Se crea una función que coloca la variable para acceder a ella.
 
     public void setHeroeAgil(int damage)
     {
          HeroeAgil= damage;
     }
 
+    // Esta funcion obtendra la información y la devolvera a la variable.
     public int obtenerHeroeAgil()
     {
         if (isDead && health != 0.0f) 
@@ -41,16 +47,21 @@ public class Heroe : Player
 
 public class EnemigoBase : Player
 {
+    //Está clase contiene variables protegidas.
     protected int hechicero;
     
     protected int explorador;
     protected float arquero;
     protected float caballero;
 
+    //La funcion se usa para colocar nuevamente la variable yt acceder a ella.
+
     public void sethechicero(int damage)
     {
         hechicero = damage;
     }
+
+    //Se obtiene la información para regresarla a la variable.
     public int getHechicero()
     {
         return hechicero;
@@ -60,16 +71,19 @@ public class EnemigoBase : Player
 
 public class ClaseAjena : MonoBehaviour
 {
+    // Esta clase se crean variables que acceden al nombre de la clase y su contenido.
     public Player player;
     public Heroe heroe;
     public EnemigoBase enemigo;
 
+    // La funcion se crea para usar las variables creadas y definirlas.
     void Start()
     {
        player = new Player();
        heroe = new Heroe();
        enemigo = new EnemigoBase();
 
+        // Se accede a las variables de las otras clases a traves del nombre que les dimos.
         player.isDead = false;
         heroe.setHeroeAgil(10);
         enemigo.sethechicero(20);
